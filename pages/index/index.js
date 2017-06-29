c7176af4465041a8b98b00986fa514c1080dca3e
tanjiahui <673800357@qq.com>
2017-06-29 19:25:34 +0800

    '报名系统静态页面'

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,24 +3,15 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    departmentArr:['请选择部门 ','计算机部','电子部','技术支持中心'],
+    currentSelect:'',
+    index:0
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  bindPickerChange: function(e){
+    this.setData({
+      index: e.detail.value
+    });
   },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
+  onLoad:  ()=> {
   }
 })
